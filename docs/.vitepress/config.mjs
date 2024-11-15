5642f152390b000a5e6efc34940b7a850c084fd3
Pker <1070994205@qq.com>
2024-11-15 08:44:35 +0800

    1

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,8 +2,31 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  lang: 'zh-CN',
   title: "My Awesome Project",
   description: "A VitePress Site",
+  base: '/',
+
+  //markdown配置
+  markdown: {
+    //行号显示
+    lineNumbers: true,
+
+    // 使用 `!!code` 防止转换
+    codeTransformers: [
+      {
+        postprocess(code) {
+          return code.replace(/\[\!\!code/g, '[!code')
+        }
+      }
+    ],
+
+    // 开启图片懒加载
+    image: {
+      lazyLoading: true
+    }
+  },
+
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
