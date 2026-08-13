17f5e4ee68d20b9f042d1268bee42051705f3288
WinPlus <1070994205@qq.com>
2026-08-13 17:08:36 +0800

    update: navi.js, chrome.html, index.html

--- navi.js
+++ navi.js
@@ -4,14 +4,8 @@ jQuery(document).ready(function(){function c(){document.title=document[a]?"（�
 // 主页判断（已禁用，方便调试）
 // var myurl = document.domain;if (myurl != "gotoo.asia") {window.location.href = "https://gotoo.asia/";}
 
-// 百度统计代码
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?f0ea78561744f7bff2f9a3baea844631";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
+// 统计代码
+document.write("<script>!function(p){\"use strict\";!function(t){var s=window,e=document,i=p,c=\"\".concat(\"https:\"===e.location.protocol?\"https://\":\"http://\",\"sdk.51.la/js-sdk-pro.min.js\"),n=e.createElement(\"script\"),r=e.getElementsByTagName(\"script\")[0];n.type=\"text/javascript\",n.setAttribute(\"charset\",\"UTF-8\"),n.async=!0,n.src=c,n.id=\"LA_COLLECT\",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:\"3QruCLjADTM0qWii\",ck:\"3QruCLjADTM0qWii\"});</script>")
 
 // 通用复制功能
 $(function() {
